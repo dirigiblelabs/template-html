148cd94060c1992f5dd76b333e10f8ffa9ee816e
Nedelcho Delchev <delchevn@gmail.com>
2017-11-07 19:28:23 +0200

    Update template.js

--- template-html/template.js
+++ template-html/template.js
@@ -1,7 +1,7 @@
 exports.getTemplate = function() {
 	return {
-		"name": "HTML",
-		"description": "HTML Template",
+		"name": "HTML5 (AngularJS)",
+		"description": "HTML5 Template with AngularJS",
 		"sources": [{
 			"location": "/template-html/index.html.template", 
 			"action": "generate",
